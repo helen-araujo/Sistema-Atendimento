1ccadd95829aec9a4666e4fa0cb860be66e0da08
helen-araujo <helemaraujo2017@gmail.com>
2022-05-04 21:59:01 -0300

    Implementação da Função Salvar

--- main.js
+++ main.js
@@ -4,7 +4,18 @@ var nomeCliente;
 var tipoCliente;
 var funcionario;
 var servico;
+var bottonSalvar;
+var tableData;
 
+var imprimirData;
+var imprimirMatricula;
+var imprimirNomeCliente;
+var imprimirTipoCliente;
+var imprimirFuncionario;
+var imprimirServico;
+
+
+//Variaveis para receber valor input->
 data = document.querySelector('[data-id="data"]');
 matricula = document.querySelector('[data-id="matricula"]');
 nomeCliente = document.querySelector('[data-id="nomeCliente"]');
@@ -13,9 +24,27 @@ funcionario = document.querySelector('[data-id="funcionario"]');
 servico = document.querySelector('[data-id="servico"]');
 tableData = document.querySelector('[data-id="tableData"]');
 
+//Variaveis para imprimir na tela de acordo com a classe->
+imprimirData = document.querySelector('.data');
+imprimirMatricula = document.querySelector('.matricula');
+imprimirTipoCliente = document.querySelector('.tipoCliente');
+imprimirNomeCliente = document.querySelector('.nomeCliente');
+imprimirServico = document.querySelector('.servico');
+imprimirFuncionario = document.querySelector('.funcionario');
+
+console.log('Teste');
+bottonSalvar = document.querySelector('[data-id="bottonSalvar"]');
+
+bottonSalvar.addEventListener('click', salvar);
+
 function salvar() {
-    tableData.textContent = data.value;
-    console.log('ok')
+    imprimirData.textContent = data.value;
+    imprimirMatricula.textContent = matricula.value;
+    //Passar valores de uma tag selector->
+    imprimirTipoCliente = tipoCliente.options[tipoCliente.selectedIndex].imprimirTipoCliente;
+    imprimirNomeCliente.textContent = nomeCliente.value;
+    imprimirServico.textContent = servico.value;
+    imprimirFuncionario = funcionario.options[funcionario.selectedIndex].imprimirFuncionario;
+    console.log('Informações registradas');
 
 }
-salvar.addEventListener('click', salvar);
